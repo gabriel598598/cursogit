be21b981ee91feb71371893825b2db8dc082188a
gabriel poot <gabriel_mau598@hotmail.com>
2023-12-11 09:49:04 -0600

    se modifico la carpeta regresar_Asiento_Borrador.py con el archivo pp.py, se agrego la carpeta vacaciones prescritas con los archivos base_pro_vacaciones_presc.py, base_staging1_vacaciones_presc.py, ultimate_staging1_vacaciones_presc.py. se modifico el archivo vacaciones_precritas_base.py

--- Regresar_Asiento_Borrador.py/Regresar_Asiento_Borrador.py
+++ Regresar_Asiento_Borrador.py/Regresar_Asiento_Borrador.py
@@ -53,20 +53,22 @@ if env.user.id.has_group(276):
         move.mapped('line_ids.analytic_line_ids').unlink()
     record.mapped('line_ids').remove_move_reconcile()
     record.write({'state': 'draft', 'is_move_sent': False})
-else:
-      raise Warning('No tiene permisos')
+else:   
+      raise Warning('No tiene permisos ')
   #-------------------------------prime-------------------------------------
 if env.user.has_group('__export__.res_groups_277_b093c683'):
     AccountMoveLine = record.env['account.move.line']
     excluded_move_ids = []
     if record._context.get('suspense_moves_mode'):
         excluded_move_ids = AccountMoveLine.search(AccountMoveLine._get_suspense_moves_domain() + [('move_id', 'in', record.ids)]).mapped('move_id').ids
+    
     for move in record:
         if move in move.line_ids.mapped('full_reconcile_id.exchange_move_id'):
             raise UserError(('No puedes reestablecer a borrador un asiento de diario de diferencia de tipo de cambio.'))
         if move.tax_cash_basis_rec_id:
             raise UserError(('No puedes reestablecer a borrador un asiento de diario de base de impuestos de efectivo.'))
         if move.restrict_mode_hash_table and move.state == 'posted' and move.id not in excluded_move_ids:
+           
             raise UserError(('No puede modificar un asiento publicado de este diario porque está en modo estricto.'))
         # We remove all the analytics entries for this journal
         move.mapped('line_ids.analytic_line_ids').unlink()
